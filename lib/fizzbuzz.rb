662eff3374e9d8434a37bab8aaad796694b48db1
meta-morpho-sys <yuliya.nedyalkova777@gmail.com>
2017-06-29 11:38:46 +0100

    First fizzbuzz rspec and solution

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,3 +1,21 @@
+=begin
+# The program can be passed a number.
+# When passed a number that is a multiple of 3, the program returns the message 'Fizz'.
+# When passed a number that is a multiple of 5, the program returns the message 'Buzz'.
+#  When passed a number that is a multiple of both 3 and 5, the program ignores the previous 2 rules and \
+# returns the message 'Fizzbuzz'.
+# In all other cases, the program simply returns the given number.
+=end
+
 def fizzbuzz(number)
-  'fizz'
+  if number % 15 == 0  # If number is divisible by 15
+    'fizzbuzz'
+  elsif number % 3 == 0 # If number is divisible by 3
+    'fizz'
+  elsif number % 5 == 0  #  If number is divisible by 5
+    'buzz'
+  else
+    number
+  end
 end
+
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,7 +1,49 @@
+=begin
+# The program can be passed a number.
+# When passed a number that is a multiple of 3, the program returns the message 'Fizz'.
+# When passed a number that is a multiple of 5, the program returns the message 'Buzz'.
+#  When passed a number that is a multiple of both 3 and 5, the program ignores the previous 2 rules and \
+# returns the message 'Fizzbuzz'.
+# In all other cases, the program simply returns the given number.
+=end
+
 require 'fizzbuzz'
 
 describe 'fizzbuzz' do
-  it 'returns fizz when passed 3' do
-    expect(fizzbuzz(3)).to eq 'fizz'
+  it 'returns fizz when number is a multiple of 3' do
+    fizz_3 = fizzbuzz(3)
+    expect(fizz_3).to eq 'fizz'
+  end
+
+  it 'return buzz when number is a multiple of 5' do
+    fizz_5 = fizzbuzz(5)
+    expect(fizz_5).to eq 'buzz'
+  end
+
+  it 'return fizzbuzz when number is a multiple of 15' do
+    fizz_5_and_3 = fizzbuzz(15)
+    expect(fizz_5_and_3).to eq 'fizzbuzz'
+  end
+
+  it 'returns the given number for one other case' do
+    number = 1
+    other = fizzbuzz(number)
+    expect(other).to eq number
+  end
+
+  it 'returns the given number for many other cases' do
+    (0..100).each do |number|
+      other = fizzbuzz(number)
+#      expect(other).to eq number if other != 'fizz' && other != 'fizzbuzz' && other != 'buzz'
+      expect(other).to eq number unless other =~ /fizz|buzz/
+    end
+  end
+
+  it 'returns the given number for many other cases (in another way)' do
+    other_numbers = (0..100).select { |n| n%3 != 0 && n%5 != 0 }
+    other_numbers.each do |number|
+      other = fizzbuzz(number)
+      expect(other).to eq number
+    end
   end
 end
